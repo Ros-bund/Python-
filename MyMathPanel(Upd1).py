a6e1d5a46da0cc542659ab06b276aba11fcf241a
Ros-bund <rostikbondar5@gmail.com>
2025-12-08 20:49:26 +0300

    Add files via upload

---
+++ MyMathPanel(Upd1).py
@@ -0,0 +1,59 @@
+import time
+from math import *
+print('=====MathPanel=====')
+print()
+
+print('1--возведение числа')
+print('2--округление числа до ближайшего наибольшего целого')
+print('3--округление числа до ближайшего наименьшего целого')
+print('4--факториал числа')
+print('5--квадратный корень')
+print()
+
+X = int(input('Pls input number 1-4: '))
+print()
+Num = float(input('Pls input Your number: '))
+print()
+
+if X < 1 and X > 4:
+    print('Error, input 1-5')
+elif X == 1:
+    Power1 = int(input('Pls input power pow: '))
+    P1 = pow(Num, Power1)
+    time.sleep(1)
+    print('99%...')
+    time.sleep(1)
+    print(P1)
+elif X == 2:
+    C1 = ceil(Num)
+    time.sleep(1)
+    print('67%...')
+    time.sleep(1)
+    print(C1)
+elif X == 3:
+    F1 = floor(Num)
+    time.sleep(1)
+    print('69%...')
+    time.sleep(1)
+    print(F1)
+elif X == 4:
+    Fact1 = factorial(Num)
+    time.sleep(1)
+    print('42%...')
+    time.sleep(1)
+    print(Fact1)
+elif X == 5:
+    S1 = sqrt(Num)
+    time.sleep(1)
+    print('1%...')
+    time.sleep(2)
+    print('67%...')
+    time.sleep(3)
+    print('99%...')
+    time.sleep(4)
+    print(S1)
+    
+    
+print()
+print('=====End====')
+print('goodbye!')
